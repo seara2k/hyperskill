ed10865a9369ca095ad45d9060a54ad6b3a71bae
seara2k <seara2k@users.noreply.github.com>
2020-08-27 18:36:53 +0300

    Machines have needs

---
+++ Coffee Machine/2.py
@@ -0,0 +1,5 @@
+cups = int(input('Write how many cups of coffee you will need:'))
+print('For ', cups, ' cups of coffee you will need:')
+print(cups * 200, ' ml of water')
+print(cups * 50, ' ml of milk')
+print(cups * 15, ' g of coffee beans')
